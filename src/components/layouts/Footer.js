2ef2ad1cf49ecf313ed8df12cee2b9bcbd8c22bb
john200tim-hash <john200tim@gmail.com>
2025-11-06 18:33:20 +0300

    feat: Add support number and tooltip to footer

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -11,6 +11,19 @@ export default function Footer({ onContactClick }) {
         Have a suggestion?{' '}
         <button onClick={onContactClick} className={styles.footerLink}>Contact us</button>
       </p>
+      <div className={styles.supportInfo}>
+        Support us:
+        <span className={styles.supportNumber}>0740297388
+          <span className={styles.tooltip}>
+            Why Support?<br />
+            This project is a labor of love, built in my spare time. Your support helps keep the servers running and enables future development.
+          </span>
+          <span className={styles.questionMark}>
+              ?
+          </span>
+        </span>
+      </div>
+
     </footer>
   );
 }
